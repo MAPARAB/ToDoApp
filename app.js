f4daf5d87269fd3a18af7bbe2f7b2709d6d21b80
makarand.parab@capgemini.com <makarand.parab@capgemini.com>
2016-06-20 17:29:18 +0530

    added header route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var headers = require('./routes/headers');
 
 var app = express();
 
@@ -24,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/header', header);
+app.use('/headers', headers);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
