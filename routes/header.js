ad76e12324ae4ccaac662e422c11073e2993c9b0
makarand.parab@capgemini.com <makarand.parab@capgemini.com>
2016-06-20 17:20:52 +0530

    Added new header script for json output

---
+++ routes/header.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET Header Json. */
+router.get('/header', function(req, res, next) 
+{
+	res.json(JSON.stringify(req.headers)); 
+});
+
+module.exports = router;
