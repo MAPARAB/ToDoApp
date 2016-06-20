8424bccf24fe7070fa58b6d700d306244a1b0812
makarand.parab@capgemini.com <makarand.parab@capgemini.com>
2016-06-20 17:47:39 +0530

    updated the get path

--- routes/header.js
+++ routes/header.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET Header Json. */
-router.get('/header', function(req, res, next) 
+router.get('/', function(req, res, next) 
 {
 	res.json(JSON.stringify(req.headers)); 
 });
