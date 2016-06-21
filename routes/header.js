c7e094f058e2849c8df9ea202832501c4325275a
makarand.parab@capgemini.com <makarand.parab@capgemini.com>
2016-06-21 14:29:52 +0530

    json response

--- routes/header.js
+++ routes/header.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET Header Json. */
-router.get('/', function(req, res, next){
-	res.send('respond with a resource');
+router.get('/header', function(req, res, next) 
+{
+	res.json(JSON.stringify(req.headers)); 	
 });
 
 module.exports = router;
